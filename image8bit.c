e8079554181f1fce382235246a8a49c495824818
Blosuhm <zakhar.kruptsala@gmail.com>
2023-11-17 16:24:55 +0000

    FIX (G): Assert position corrected

--- image8bit.c
+++ image8bit.c
@@ -349,10 +349,10 @@ int ImageValidRect(Image img, int x, int y, int w, int h) { ///
 // The returned index must satisfy (0 <= index < img->width*img->height)
 static inline int G(Image img, int x, int y) {
   int index;
-  assert(0 <= index && index < img->width * img->height);
   // Insert your code here!
-
   index = img->width * y + x;
+
+  assert(0 <= index && index < img->width * img->height);
   return index;
 }
 
